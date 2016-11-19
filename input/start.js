85687e445754854e398a1022043cea22e684b764
John McCracken <johnmccuk@gmail.com>
2016-11-19 10:48:53 +0000

    Initial commit

---
+++ input/start.js
@@ -0,0 +1,3 @@
+//constants are an ES6 feature and wont run in plain old js
+const PI = 3.141593;
+alert("Value is "+ PI);
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+//Define what libraries we need
+var webpack = require('webpack');
+var path = require('path');
+
+//setup the i/o paths
+var OUTPUT_DIR = path.resolve(__dirname, 'output');
+var INPUT_DIR = path.resolve(__dirname, 'input');
+
+//create a config object
+var config = {
+    entry: INPUT_DIR + '/start.js',
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            },
+            sourceMap: true
+        }),
+        new extractTextPlugin("[name].css")
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js/, //look for js files
+                include: INPUT_DIR, //in this directory
+                loader: 'babel' //run the babel loader
+            }
+        ]
+    },
+    output: {
+        path: OUTPUT_DIR,
+        filename: 'finish.js'
+    }
+};
+
+//finally make the config object available elsewhere
+module.exports = config;
